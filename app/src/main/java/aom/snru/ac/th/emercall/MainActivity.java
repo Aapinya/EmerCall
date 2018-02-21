40511e903ecf15a3593226c96d8047b060c4ef12
Admin <Apinya.po59@snru.ac.th>
2018-02-21 11:24:45 +0700

    Drawer Mnu Suce

--- app/src/main/java/aom/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/aom/snru/ac/th/emercall/MainActivity.java
@@ -1,18 +1,45 @@
 package aom.snru.ac.th.emercall;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import java.util.GregorianCalendar;
 
 import aom.snru.ac.th.emercall.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+//    Explicit
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        create Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+       getSupportActionBar().setHomeButtonEnabled(true);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       drawerLayout = findViewById(R.id. drawerLayoutMain);
+       actionBarDrawerToggle = new ActionBarDrawerToggle(
+               MainActivity.this,
+               drawerLayout,
+               R.string.open,
+               R.string.close
+       );
+       drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
 
 
 //  Add Fragment
@@ -24,4 +51,26 @@ public class MainActivity extends AppCompatActivity {
 
     }  //Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+             return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+
+        actionBarDrawerToggle.syncState();
+    }
 } //Main class
