30c841cae25bde7efa1925954131e725b5f1bb3f
Admin <Apinya.po59@snru.ac.th>
2018-02-21 14:17:25 +0700

    Lass

--- app/src/main/java/aom/snru/ac/th/emercall/fragment/MainFragment.java
+++ app/src/main/java/aom/snru/ac/th/emercall/fragment/MainFragment.java
@@ -3,9 +3,14 @@ package aom.snru.ac.th.emercall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.FormatFlagsConversionMismatchException;
 
 import aom.snru.ac.th.emercall.R;
 
@@ -14,6 +19,46 @@ import aom.snru.ac.th.emercall.R;
  */
 
 public class MainFragment extends Fragment{
+
+//    Exception
+     private  String tag = "MyTagV1";
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//          For Station 1
+
+        ImageView station1ImageView = getView().findViewById(R.id.imageViewStation1);
+        station1ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(tag,"you Click lmage station1");
+
+
+            }
+        });
+
+
+        TextView station1TextView = getView().findViewById(R.id.textViewStation1);
+        station1ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(tag, "Click Text"+ getString(R.string.station1));
+            }
+        });
+
+//          For station 2
+
+//          For  station 3
+
+//          For  Station 4
+    }//        Main Method
+
+    public  void  callStation(String numberCall){
+
+    }
+
 
     @Nullable
     @Override
